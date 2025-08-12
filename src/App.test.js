720dd41db8fe83041ead9b385fc515a34d8ae7a7
RJ123496 <rladhroya19@gmail.com>
2025-08-12 00:14:41 -0600

    Project cleanup: Remove unnecessary documentation and testing files, optimize project structure

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-import App from "./App.jsx";
-
-test("renders app without crashing", () => {
-  render(<App />);
-  expect(document.body).toBeTruthy();
-});
